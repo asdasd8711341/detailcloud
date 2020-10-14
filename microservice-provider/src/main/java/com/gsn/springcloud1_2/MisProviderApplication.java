1d4416ffd52fa9391880f1e79b6e3acf399d5acb
asdasd8711341 <995736488@qq.com>
2020-10-14 20:14:16 +0800

    SpringCloud练习项目二，实现Eureka高有效

--- microservice-provider/src/main/java/com/gsn/springcloud1_2/MisProviderApplication.java
+++ microservice-provider/src/main/java/com/gsn/springcloud1_2/MisProviderApplication.java
@@ -2,10 +2,12 @@ package com.gsn.springcloud1_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient   //启用服务发现客户端,以获取当前provider的注册信息
 public class MisProviderApplication {
 
 	public static void main(String[] args) {
